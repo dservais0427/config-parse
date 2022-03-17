b88620826582b9489fc7b771a4230dd9bb62ab9f
dservais0427 <40124987+dservais0427@users.noreply.github.com>
2022-03-17 13:19:24 -0500

    Update parse_comware_config.py

--- parse_comware_config.py
+++ parse_comware_config.py
@@ -5,7 +5,7 @@
 """
 
 __author__ = 'David Servais'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 import csv
 import os
@@ -46,7 +46,7 @@ args = parser.parse_args()
 if args.config is None:
     root = tk.Tk()
     root.withdraw()
-    config = fd.askopenfilename(title='Select Router Configuration File')
+    config = fd.askopenfilenames(title='Select Router Configuration File')
     if config == '':
         exit()
 else:
@@ -92,84 +92,84 @@ if os.path.exists('vlanmap.txt'):
 else:
     checkVlan = False
 
-confparse = CiscoConfParse(config)
+file_list = list(config)
 
-hostname = confparse.re_match_iter_typed(r'sysname\s+(\S+)', default='unknown')
-outfile = hostname.upper() + '_interfaces.csv'
-outfile = Path(os.environ['TEMP'], outfile)
+for x in file_list:
+    # Begin processing
+    confparse = CiscoConfParse(x)
 
-intf_all = []
+    hostname = confparse.re_match_iter_typed(r'sysname\s+(\S+)', default='unknown')
+    outfile = hostname.upper() + '_interfaces.csv'
+    outfile = Path(os.environ['TEMP'], outfile)
 
-# extract the interface details from config file
-# get all ethernet interfaces from the configuration file
-interface_cmds = confparse.find_objects(r'^interface(.+?thernet|.+?Bridge-Aggregation)')
+    intf_all = []
 
-# iterate over the resulting IOSCfgLine objects and parse out desired info
-for interface_cmd in interface_cmds:
-    intf = ['', '', '', '', '', '', '', '', '', '', '']
-    vlanCmd = []
-    exCmd = []
-    stpCmd = []
-    portsecCmd = []
-    sdplxCmd = []
-    qosCmd = []
+    # extract the interface details from config file
+    # get all ethernet interfaces from the configuration file
+    interface_cmds = confparse.find_objects(r'^interface(.+?thernet|.+?Bridge-Aggregation)')
 
-    # get the interface name
-    intf_name = interface_cmd.text[len('interface '):]
-    intf.insert(0, intf_name)
+    # iterate over the resulting IOSCfgLine objects and parse out desired info
+    for interface_cmd in interface_cmds:
+        intf = ['', '', '', '', '', '', '', '', '', '', '']
+        vlanCmd = []
+        exCmd = []
+        stpCmd = []
+        portsecCmd = []
+        sdplxCmd = []
+        qosCmd = []
 
-    # search for the description command
-    for cmd in interface_cmd.re_search_children(r'^\sdescription'):
-        intf.insert(3, cmd.text.strip()[12:])
+        # get the interface name
+        intf_name = interface_cmd.text[len('interface '):]
+        intf.insert(0, intf_name)
 
-    # determine if this is a trunk or access port and capture specific info
-    for cmd in interface_cmd.re_search_children(r'^\sport\slink-type'):
+        # search for the description command
+        for cmd in interface_cmd.re_search_children(r'^\sdescription'):
+            intf.insert(3, cmd.text.strip()[12:])
 
-        if cmd.text == ' port link-type trunk':
-            cmd1 = interface_cmd.re_match_iter_typed(r'\sport\strunk\snative', default='vlan 1')
-            intf.insert(4, cmd1.strip())
+        # determine if this is a trunk or access port and capture specific info
+        for cmd in interface_cmd.re_search_children(r'^\sport\slink-type'):
 
-            # cmd2 = interface_cmd.re_match_iter_typed(r'\sport\strunk\spermit', default='vlan all')
-            # intf.insert(5, cmd2.strip())
+            if cmd.text == ' port link-type trunk':
+                cmd1 = interface_cmd.re_match_iter_typed(r'\sport\strunk\snative', default='vlan 1')
+                intf.insert(4, cmd1.strip())
 
-    for cmd1 in interface_cmd.re_search_children(r'^\sport\saccess\svlan'):
-        intf.insert(4, cmd1.text.strip())
+        for cmd1 in interface_cmd.re_search_children(r'^\sport\saccess\svlan'):
+            intf.insert(4, cmd1.text.strip())
 
-    for cmd in interface_cmd.re_search_children(r'^\sshutdown'):
-        intf.insert(6, cmd.text.strip())
+        for cmd in interface_cmd.re_search_children(r'^\sshutdown'):
+            intf.insert(6, cmd.text.strip())
 
-    for cmd in interface_cmd.re_search_children(r"^\sspeed|^\sduplex"):
-        sdplxCmd.append(cmd.text.strip())
+        for cmd in interface_cmd.re_search_children(r"^\sspeed|^\sduplex"):
+            sdplxCmd.append(cmd.text.strip())
 
-    # Process additional commands
-    for cmd in interface_cmd.re_search_children(r'\sport\slink-aggregation'):
-        exCmd.append(cmd.text.strip())
+        # Process additional commands
+        for cmd in interface_cmd.re_search_children(r'\sport\slink-aggregation'):
+            exCmd.append(cmd.text.strip())
 
-    for cmd in interface_cmd.re_search_children(r'^\sstp'):
-        stpCmd.append(cmd.text.strip())
+        for cmd in interface_cmd.re_search_children(r'^\sstp'):
+            stpCmd.append(cmd.text.strip())
 
-    intf.insert(7, ','.join(sdplxCmd))
-    intf.insert(8, ','.join(exCmd))
-    intf.insert(9, ','.join(stpCmd))
-    intf.insert(10, ','.join(qosCmd))
-    intf.insert(11, ','.join(portsecCmd))
-    
-    # add interface info to the all interface list
-    intf_all.append(intf)
+        intf.insert(7, ','.join(sdplxCmd))
+        intf.insert(8, ','.join(exCmd))
+        intf.insert(9, ','.join(stpCmd))
+        intf.insert(10, ','.join(qosCmd))
+        intf.insert(11, ','.join(portsecCmd))
 
+        # add interface info to the all interface list
+        intf_all.append(intf)
 
-# Output data to CSV file
-with open(outfile, 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, dialect='excel')
-    writer.writerow(['Interface', 'Switch Name', 'New Interface', 'Type',
-                     'Description', 'Access/Native', 'Voice/Allowed',
-                     'Shutdown', 'Speed/Duplex', 'Commands', 'STP',
-                     'QOS', 'Port Security'])
+    # Output data to CSV file
+    with open(outfile, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, dialect='excel')
+        writer.writerow(['Interface', 'Switch Name', 'New Interface', 'Type',
+                         'Description', 'Access/Native', 'Voice/Allowed',
+                         'Shutdown', 'Speed/Duplex', 'Commands', 'STP',
+                         'QOS', 'Port Security'])
 
-    for x in intf_all:
-        writer.writerow([x[0], '', x[1], checkKey(vlans, x[4]),
-                         x[3], x[4], x[5], x[6], x[7], x[8], x[9],
-                         x[10], x[11]])
+        for x in intf_all:
+            writer.writerow([x[0], '', x[1], checkKey(vlans, x[4]),
+                             x[3], x[4], x[5], x[6], x[7], x[8], x[9],
+                             x[10], x[11]])
 
-# open csv file for review
-os.startfile(outfile)
+    # open csv file for review
+    os.startfile(outfile)
